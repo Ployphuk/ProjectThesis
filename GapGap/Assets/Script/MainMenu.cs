9f584d0d1708ac7cb3cde2908aa71b8b9d3fdd43
Ployphuk <ployphuk.jafarx@mail.kmutt.ac.th>
2024-08-11 21:26:14 +0700

    Add : scenece transition

--- GapGap/Assets/Script/MainMenu.cs
+++ GapGap/Assets/Script/MainMenu.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Animator transition;
+    public GameObject SceneObject;
+    public void Awake()
+    {
+        SceneObject.SetActive(false);
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneObject.SetActive (true);
+        StartCoroutine(NextLevel());
     }
+
+    IEnumerator NextLevel()  // Fixed return type and method name
+    {
+        transition.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        transition.SetTrigger("Start");
+    }
+
     public void Quit()
     {
         Application.Quit();
